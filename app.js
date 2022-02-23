4b3941176110776d3a5f51364f1b0dd45abc47f5
M1571 <94639639+M1571@users.noreply.github.com>
2022-02-23 18:29:38 +0000

    set img

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 // console.log('prova');
 
-// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data
+/*
+Descrizione:
+Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+Bonus:
+Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+*/
 
 // -----------------------------------
+
