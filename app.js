0e591a1422a4a100e52435bb4d92d14363e17cc0
M1571 <94639639+M1571@users.noreply.github.com>
2022-02-23 22:36:37 +0000

    fine

--- app.js
+++ app.js
@@ -9,3 +9,12 @@ Aggiungere alla pagina un’immagine, presa anch’essa da un data.
 
 // -----------------------------------
 
+let app = new Vue (
+    {
+        el: "#container",
+        data: {
+            message: "Hi tutor!",
+            image: "https://picsum.photos/700/400/"
+        }
+    }
+);
